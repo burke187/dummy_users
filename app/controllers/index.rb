802e5d24b120758645d79f116db0c0889fe8e49f
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 09:41:15 -0500

    added create user route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,22 @@
+enable :sessions
+
 get '/' do
   
   erb :index
 end
 
-post '/' do
-
+post '/create' do
+ user = User.create(params[creation])
+ session[:user] = user
+ redirect('/secret')
 end
 
+get '/secret' do
+  if session[:user]
+    erb :secret
+  else
+    erb :index
+end
+end
 # params[creation] = { user_name: <whatever they said>, }
 # User.create(params[creation])
